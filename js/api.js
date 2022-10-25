4a9deb7ced9c6c20003442c3ac31afd448d94c85
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2022-10-25 18:13:37 +0530

    word limit

--- js/api.js
+++ js/api.js
@@ -32,7 +32,7 @@ function showMovies(data){
         <div class="mx-2 relative" style="min-height: 208px;min-width: 226px;">
         <img class="movie-list-item-img w-96 h-52 rounded-xl " src="${IMG_URL+poster_path}" alt="">
         <div class="flex">
-            <span class="movie-list-item-title bg-slate-600 px-1 text-2xl  top-9 left-10 z-10 absolute">${title}</span>
+            <span class="movie-list-item-title bg-slate-600 px-1 text-2xl  top-9 left-10 z-10 absolute">${title.slice(0,10)}</span>
             <span class="rating top-9 right-2.5 z-10 absolute  bg-green-500 w-14 rounded-lg cursor-pointer">90</span>
         </div>
         <p class="featured-desc w-6/12 text-gray-300 my-2  bg-slate-600 w-9/12 top-16 left-10 absolute text-xs" >${overview.slice(0,70)}....</p>
